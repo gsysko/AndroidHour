160c58da755100e9cc09d1ef64a7dd1879f631a0
banelson <hil13190>
2013-07-02 15:59:40 -0400

    Added section covering intents (implicit and explicit)

--- AndroidHour/src/main/java/com/uxd/androidhour/MainActivity.java
+++ AndroidHour/src/main/java/com/uxd/androidhour/MainActivity.java
@@ -3,14 +3,10 @@ package com.uxd.androidhour;
 
 import android.app.ActionBar;
 import android.os.Bundle;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.NavUtils;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -99,6 +95,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.OnNaviga
                 .replace(R.id.container, fragment)
                 .commit();
         return true;
+
     }
 
     /**
